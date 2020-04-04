9ebc72be59b1d1fdf29a48f5d033152961b6795d
Charbel Harb <s@scorz.org>
2020-04-04 03:09:05 +0300

    init

---
+++ WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Core;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : BaseController
+    {
+
+    }
+}
---
+++ WebAPI/Core/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Core
+{
+    [Route("api/[controller]")]
+    public class BaseController : Controller
+    {
+
+    }
+}
